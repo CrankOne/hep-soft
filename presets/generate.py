9ef01097004df0bca46a30f5b80c3373b0a0c6d8
Renat R. Dusaev <crank@qcrypt.org>
2020-02-26 20:26:00 +0700

    ~ on generate.py script

--- presets/generate.py
+++ presets/generate.py
@@ -3,12 +3,30 @@ import sys
 import re
 import yaml
 
+# Regular expression for the package atom entry
 rxsPortageAtom = r'^(?P<verQuantify>[<>]?[=])?(?P<pkgGroup>[\w\-]+)/(?P<pkgName>[\w\-]+\w)(?:-(?P<pkgVer>[\d.]+))?$'
 
+class PersistentFile(object):
+
+    def __init__(self, filename, mode='r'):
+        self._fName = filename
+        self._fOpenMode = mode
+
+    def __enter__(self):
+        os.makedirs( os.path.dirname( self._fName ), exist_ok=True )
+        self._f = open(self._fName, self._fOpenMode)
+        return self._f
+
+    def __exit__(self, exc_type, exc_value, excTraceback):
+        self._f.close()
+        self._f = None
 
 def main( specFileName, baseDir='2rem' ):
+    """
+    Main entry point, deploys file tree.
+    """
     # Parse specification
-    with open(sys.argv[1], 'r') as f:
+    with open(specFileName, 'r') as f:
         try:
             spec = yaml.safe_load(f)
         except yaml.YAMLError as exc:
@@ -19,8 +37,7 @@ def main( specFileName, baseDir='2rem' ):
         setFileName = os.path.join( baseDir, 'etc/portage/sets', setName )
         pkgNames = sorted(spec[setName].keys())
         # Write set file
-        os.makedirs( os.path.dirname( setFileName ), exist_ok=True )
-        with open( setFileName, 'w' ) as setFile:
+        with PersistentFile( setFileName, 'w' ) as setFile:
             setFile.write('\n'.join(pkgNames))
         # Write useflags files (taking deps' use into account)
         # ...
