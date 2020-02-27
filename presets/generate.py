611728d1baeb902f9c4b32545c3a1f15cbc728a2
Crank <crank@qcrypt.org>
2020-02-27 10:01:06 +0700

    ~ on generate.py: daily update

--- presets/generate.py
+++ presets/generate.py
@@ -2,24 +2,68 @@ import os
 import sys
 import re
 import yaml
+import io
 
 # Regular expression for the package atom entry
-rxsPortageAtom = r'^(?P<verQuantify>[<>]?[=])?(?P<pkgGroup>[\w\-]+)/(?P<pkgName>[\w\-]+\w)(?:-(?P<pkgVer>[\d.]+))?$'
+rxPortageAtom = re.compile(r'^(?P<quantify>[<>]?=?)?(?P<group>[\w\-]+)/(?P<name>[\w\-]+\w)(?:-(?P<ver>[\d.]+))?(?:(?P<patch>.+)?)$')
 
-class PersistentFile(object):
+def get_atom_dict( atomStrExpr ):
+    """
+    Applies regular expression to Gentoo Portage's package atom string
+    veryfying match result and returning groups dictionary.
+    """
+    m = rxPortageAtom.match(atomStrExpr)
+    if not m:
+        raise RuntimeError("Exprssion \"%s\" does not seem to be a valid package atom.")
+    return dict(m.groupdict())
 
-    def __init__(self, filename, mode='r'):
+class AtomRef(object):
+    """
+    Configuration entry referencing Gentoo Portages atom together with
+    subsidiary information: keywords, useflags, licences, etc.
+    """
+    def __init__(self, name, payload=None, partOfSet=None):
+        self.name = name
+        self._d = get_atom_dict(name)
+        self._payload = payload if payload else dict()
+        self._deps = [ AtomRef(*item) for item in self._payload['deps'].items() ] \
+                     if 'deps' in self._payload else []
+        self._partOfSet = partOfSet
+
+    @property
+    def cfgFileNamePat(self):
+        toks = [ self._d[n] for n in ('group', 'name', 'ver', 'patch') if self._d[n] ]
+        if self._partOfSet:
+            toks = [self._partOfSet,] + toks
+        return '-'.join(toks)
+
+    def write_props_to(self, propName, f):
+        if propName in self._payload:
+            f.write( '%s %s\n'%( self.name, ' '.join(self._payload[propName]) ) )
+        for depAtom in self._deps:
+            depAtom.write_props_to(propName, f)
+
+class SmartConfig(object):
+    """
+    A "smart" file entity. Provides two features:
+        * creates file only if there were some writing in it, on "file" close
+        * recursively creates directories within the path of the file
+    """
+    def __init__(self, filename, mode='w'):
         self._fName = filename
         self._fOpenMode = mode
 
     def __enter__(self):
-        os.makedirs( os.path.dirname( self._fName ), exist_ok=True )
-        self._f = open(self._fName, self._fOpenMode)
-        return self._f
+        self._io = io.StringIO()
+        return self._io
 
     def __exit__(self, exc_type, exc_value, excTraceback):
-        self._f.close()
-        self._f = None
+        if self._io.getvalue():
+            os.makedirs( os.path.dirname( self._fName ), exist_ok=True )
+            with open(self._fName, self._fOpenMode) as f:
+                f.write( self._io.getvalue() )
+        self._io.close()
+
 
 def main( specFileName, baseDir='2rem' ):
     """
@@ -37,14 +81,16 @@ def main( specFileName, baseDir='2rem' ):
         setFileName = os.path.join( baseDir, 'etc/portage/sets', setName )
         pkgNames = sorted(spec[setName].keys())
         # Write set file
-        with PersistentFile( setFileName, 'w' ) as setFile:
+        with SmartConfig( setFileName ) as setFile:
             setFile.write('\n'.join(pkgNames))
-        # Write useflags files (taking deps' use into account)
-        # ...
-        # Write keywords files (taking deps' use into account)
-        # ...
-        # TODO: append (?) env file
-        # ...
+        for pkgName, pkgSpec in spec[setName].items():
+            pkg = AtomRef(pkgName, pkgSpec, setName)
+            for prop in ( 'keywords', 'license', 'mask', 'use' ):
+                pt = os.path.join( baseDir, 'etc/portage/package.%s'%prop, pkg.cfgFileNamePat )
+                with SmartConfig(pt, 'w') as f:
+                    pkg.write_props_to(prop, f)
+            # TODO: append (?) env file
+            # ...
 
 if "__main__" == __name__:
     sys.exit(main( sys.argv[1], sys.argv[2] ))
